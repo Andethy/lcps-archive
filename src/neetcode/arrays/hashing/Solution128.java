7011c1af9eadb066c1256fa2607349261bec69db
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2023-10-04 17:32:10 -0400

    Added solutions 36, 128

---
+++ src/neetcode/arrays/hashing/Solution128.java
@@ -0,0 +1,33 @@
+package neetcode.arrays.hashing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution128 {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        Set<Integer> hash = new HashSet<>();
+        for (int num : nums) {
+            hash.add(num);
+        }
+
+        int max = 0;
+        for (int num : nums) {
+            int count = 1;
+            if (hash.contains(num - count)) {
+                continue;
+            }
+            while (hash.contains(num + count)) {
+                count++;
+            }
+            max = Math.max(max, count);
+            // CRUCIAL for runtime
+            if (max > nums.length / 2) {
+                break;
+            }
+        }
+        return max;
+    }
+}
---
+++ src/neetcode/arrays/hashing/Solution36.java
@@ -0,0 +1,30 @@
+package neetcode.arrays.hashing;
+
+import java.util.HashSet;
+
+class Solution36 {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] subs = new HashSet[9];
+
+        for (int n = 0; n < 9; n++) {
+            rows[n] = new HashSet<>();
+            cols[n] = new HashSet<>();
+            subs[n] = new HashSet<>();
+        }
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                int sub = (c / 3 + 1) + 3 * (r / 3) - 1;
+                Character ch = board[r][c];
+                if (ch.equals('.')) {
+                    continue;
+                } else if (!rows[r].add(ch) || !cols[c].add(ch) || !subs[sub].add(ch)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
