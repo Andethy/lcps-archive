7663f1f75a041e6251e2c54bcac09e1da55a2225
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2023-10-05 23:46:08 -0400

    Cleared main

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,6 @@ import java.util.Stack;
  */
 public class Main {
     public static void main(String[] args) {
-        Integer x = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        int[] y = new int[10];
-        y.clone();
-        Stack<Character> stack = new Stack<>();
 
     }
 }
