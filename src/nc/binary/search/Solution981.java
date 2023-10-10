a1de93c2dab6afa2e867503f5588f266599974df
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2023-10-10 03:04:40 -0400

    Added solution 981

---
+++ src/nc/binary/search/Solution981.java
@@ -0,0 +1,61 @@
+package nc.binary.search;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution981 {
+    /**
+     * Your TimeMap object will be instantiated and called as such:
+     * TimeMap obj = new TimeMap();
+     * obj.set(key,value,timestamp);
+     * String param_2 = obj.get(key,timestamp);
+     */
+    class TimeMap {
+        private class CustomEntry<K, V> {
+            public K key;
+            public V val;
+
+            CustomEntry(K key, V val) {
+                this.key = key;
+                this.val = val;
+            }
+        }
+
+        HashMap<String, List<CustomEntry<String, Integer>>> map;
+
+        public TimeMap() {
+            map = new HashMap<>();
+        }
+
+        public void set(String key, String value, int timestamp) {
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(new CustomEntry<>(value, timestamp));
+        }
+
+        public String get(String key, int timestamp) {
+            List<CustomEntry<String, Integer>> list = map.get(key);
+            String toReturn = "";
+            return list == null ? "" : binarySearch(list, timestamp, 0, list.size() - 1);
+        }
+
+        private String binarySearch(List<CustomEntry<String, Integer>> list, int target, int front, int back) {
+            if (front > back) {
+                return front == 0 ? "" : list.get(front - 1).key;
+            }
+            int mid = (front + back) / 2;
+            CustomEntry<String, Integer> midEntry = list.get(mid);
+            if (midEntry.val == target) {
+                return midEntry.key;
+            } else if (midEntry.val > target) {
+                return binarySearch(list, target, front, mid - 1);
+            } else {
+                return binarySearch(list, target, mid + 1, back);
+            }
+        }
+    }
+
+
+}
